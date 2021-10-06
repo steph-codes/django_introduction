2fcceef90220be79f09b7d5e51f229c74dcf57f8
Ogundele Stephen Babatunde <ogundele370@gmail.com>
2021-10-06 22:16:19 +0100

    first commit

---
+++ storefront/text.py
@@ -0,0 +1,7 @@
+# Hmmm.. this feels like it might be a VSCode problem.
+
+# To fix the issue, you can simply open a Powershell window as administrator and type the following:
+
+#copy->      Set-ExecutionPolicy RemoteSigned
+
+# And hit enter, respond y to any prompts that appear.
